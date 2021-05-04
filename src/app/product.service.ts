46a9ea9554e43788b5ec61a3bac0c3682ad8e1b1
joaocosta99 <horaciomanuel0@gmail.com>
2021-05-04 17:09:36 +0100

    4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,10 +9,9 @@ export class ProductService {
 
   constructor(private _http: HttpClient) { }
 
-  getAlbum(id:number){
-    return this._http.get(this._albumUrl).map((response) => 
-    response.json());
+ getAlbum(id:number){
+     return this._http.get(this._albumUrl).map((response)=>
+     response.json());
+ }
 
   }
-
-}
